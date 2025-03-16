c096a7ba6da7858f6e06781444550042f0fb4cfe
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-16 18:16:27 +0300

    restructuring node files

--- Node/app/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-// app/routes/orderRoutes.js
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-
-router.post('/orders', orderController.createOrder);
-router.get('/orders', orderController.getOrders);
-
-module.exports = router;
